4fca4445d87cc58ce9ac55c210ad818ef3ac5d22
Chigozirim Uwaezuoke <Chigozirimuwaezuoke@gmail.com>
2023-08-11 18:42:43 -0700

    EOF Signal

--- console.py
+++ console.py
@@ -77,3 +77,9 @@ class HBNBCommand(cmd.Cmd):
     def do_quit(self, arg):
         """Quit command to exit the program."""
         return True
+
+    def do_EOF(self, arg):
+        """The EOF signal to exit the program."""
+        print("")
+        return True
+
