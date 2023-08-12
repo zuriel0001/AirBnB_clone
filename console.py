3c4bc52e0a5d58c5c9dad6fc595aa9686da5a11b
Chigozirim Uwaezuoke <Chigozirimuwaezuoke@gmail.com>
2023-08-12 05:44:34 -0700

    class instance

--- console.py
+++ console.py
@@ -83,3 +83,16 @@ class HBNBCommand(cmd.Cmd):
         print("")
         return True
 
+    def do_create(self, arg):
+        """
+        Create a new class instance and print its id.
+        """
+        argl = parse(arg)
+        if len(argl) == 0:
+            print("** class name missing **")
+        elif argl[0] not in HBNBCommand.__classes:
+            print("** class doesn't exist **")
+        else:
+            print(eval(argl[0])().id)
+            storage.save()
+
