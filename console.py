4d006a60cd4b49e37b7e8da8a3c16259bfe17fdb
Chigozirim Uwaezuoke <Chigozirimuwaezuoke@gmail.com>
2023-08-11 18:30:15 -0700

    parse function

--- console.py
+++ console.py
@@ -11,3 +11,21 @@ from models.city import City
 from models.place import Place
 from models.amenity import Amenity
 from models.review import Review
+
+
+def parse(arg):
+    my_curly_braces = re.search(r"\{(.*?)\}", arg)
+    my_brackets = re.search(r"\[(.*?)\]", arg)
+    if my_curly_braces is None:
+        if my_brackets is None:
+            return [i.strip(",") for i in split(arg)]
+        else:
+            lexer = split(arg[:brackets.span()[0]])
+            retl = [i.strip(",") for i in lexer]
+            retl.append(brackets.group())
+            return retl
+    else:
+        lexer = split(arg[:my_curly_braces.span()[0]])
+        retl = [i.strip(",") for i in lexer]
+        retl.append(my_curly_braces.group())
+        return retl
