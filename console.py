7c8d8975a205e68810d0cf5eccabfba3240fb529
Chigozirim Uwaezuoke <Chigozirimuwaezuoke@gmail.com>
2023-08-12 06:09:55 -0700

    display string representation

--- console.py
+++ console.py
@@ -96,3 +96,20 @@ class HBNBCommand(cmd.Cmd):
             print(eval(argl[0])().id)
             storage.save()
 
+    def do_show(self, arg):
+        """Usage: show <class> <id> or <class>.show(<id>)
+        Function that displays the string representation of a class
+        instance of a given id.
+        """
+        argl = parse(arg)
+        obj_dict = storage.all()
+        if len(argl) == 0:
+            print("** class name missing **")
+        elif argl[0] not in HBNBCommand.__classes:
+            print("** class doesn't exist **")
+        elif len(argl) == 1:
+            print("** instance id missing **")
+        elif "{}.{}".format(argl[0], argl[1]) not in obj_dict:
+            print("** no instance found **")
+        else:
+            print(obj_dict["{}.{}".format(argl[0], argl[1])])
