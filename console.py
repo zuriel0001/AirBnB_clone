379cc975527db16e2dffa578c96d743d8eb88116
Chigozirim Uwaezuoke <Chigozirimuwaezuoke@gmail.com>
2023-08-12 06:44:21 -0700

    delete a class instance

--- console.py
+++ console.py
@@ -113,3 +113,20 @@ class HBNBCommand(cmd.Cmd):
             print("** no instance found **")
         else:
             print(obj_dict["{}.{}".format(argl[0], argl[1])])
+
+    def do_destroy(self, arg):
+        """Usage: destroy <class> <id> or <class>.destroy(<id>)
+        Function that deletes a class instance of a given id."""
+        argl = parse(arg)
+        obj_dict = storage.all()
+        if len(argl) == 0:
+            print("** class name missing **")
+        elif argl[0] not in HBNBCommand.__classes:
+            print("** class doesn't exist **")
+        elif len(argl) == 1:
+            print("** instance id missing **")
+        elif "{}.{}".format(argl[0], argl[1]) not in obj_dict.keys():
+            print("** no instance found **")
+        else:
+            del obj_dict["{}.{}".format(argl[0], argl[1])]
+            storage.save()
